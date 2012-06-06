703f3e9526ec388830520b82fa04d5c9cadf7e5d
chenchunyi <ayi@creampecan.com>
2012-06-06 22:06:23 +0800

    Change to Ruby 1.9-style hashsyntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+# Returns the full title on a per-page basis.
+	def full_title(page_title)
+		base_tile = 'Ruby on Rails Tutorial Sample App'
+		if page_title.empty?
+			base_tile
+		else
+			"#{base_tile} | #{page_title}"
+		end
+	end
 end
