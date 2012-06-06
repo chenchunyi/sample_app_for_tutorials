25d93635c7e66ed3004282877785546803b1955c
chenchunyi <ayi@creampecan.com>
2012-06-07 03:06:16 +0800

    Finish layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,10 @@
+# def full_title(page_title)
+# 	base_tile = 'Ruby on Rails Tutorial Sample App'
+# 	if page_title.empty?
+# 		base_tile
+# 	else
+# 		"#{base_tile} | #{page_title}"
+# 	end
+# end
+
+include ApplicationHelper
